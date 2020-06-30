a71ed26f021b6a0e81d8519c78625eabe2df50fc
Spencer Christiansen <jc.spencer92@gmail.com>
2020-06-30 10:40:56 -0600

    kanjidamage: Improve docs and fix some small stuff

--- src/anki_from_kanjidamage.rb
+++ src/anki_from_kanjidamage.rb
@@ -1,4 +1,8 @@
-# frozen_string_literals: true
+# frozen_string_literal: true
+
+# Scrape KanjiDamage.com and create a JSON file of the data
+#
+# Use a functional style for everything.
 
 require 'mechanize'
 require 'json'
@@ -57,12 +61,12 @@ def radical_component(radical)
   { kind: 'radical', radical: radical }
 end
 
-# @param val - The thing to test
+# @param val [any] - The thing to test
 # @param msg [String] - message when the thing is not what you expect
-# @param expected - what you expect, either a Class, or a specific thing
+# @param expected [any] - what you expect, either a Class, or a specific thing
 # @yield when there is no error, yield to an optional block
 # @yieldreturn [Array<String>] - the block should return other error messages
-# @return [Array<String>] - any error message
+# @return [Array<String>] - an array of error messages
 def obj_is(val, msg, expected)
   is_klass = expected.is_a?(Class)
   if is_klass ? val.is_a?(expected) : val == expected
@@ -72,6 +76,12 @@ def obj_is(val, msg, expected)
   end
 end
 
+# @param hash [Hash] - The hash containing the thing to test
+# @param attr [any] - The key to the thing to test
+# @param thing [any] - what you expect @see `obj_is` param `expected`
+# @yield [val] when there is no error, yield `hash[attr]` to an optional block
+# @yieldreturn [Array<String>] - the block should return other error messages
+# @return [Array<String>] - an array of error messages
 def attr_is(hash, attr, thing)
   val = hash[attr]
   obj_is(val, "#{attr} to be #{thing}", thing).yield_self do |a|
@@ -79,36 +89,50 @@ def attr_is(hash, attr, thing)
   end
 end
 
+# @param comp [any]
+# @return [Array<String>]
 def kanji_component_errors(comp)
   obj_is(comp, 'a kanji_component to be a Hash', Hash) do
     attr_is(comp, :kind, 'kanji') { attr_is(comp, :kanji, String) }
   end
 end
 
+# @param component [any]
+# @return [Boolean]
 def valid_kanji_component?(component)
   kanji_component_errors(component).empty?
 end
 
+# @param comp [any]
+# @return [Array<String>]
 def radical_component_errors(comp)
   obj_is(comp, 'a radical_component to be a Hash', Hash) do
     attr_is(comp, :kind, 'radical') { attr_is(comp, :radical, String) }
   end
 end
 
+# @param component [any]
+# @return [Boolean]
 def valid_radical_component?(component)
   radical_component_errors(component).empty?
 end
 
+# @param component [any]
+# @return [Array<String>]
 def component_errors(component)
   return [] if valid_component?(component)
 
   kanji_component_errors(component) + radical_component_errors(component)
 end
 
+# @param component [any]
+# @return [Boolean]
 def valid_component?(component)
   valid_kanji_component?(component) || valid_radical_component?(component)
 end
 
+# @param jukugo [any]
+# @return [Array<String>]
 def jukugo_errors(jukugo)
   obj_is(jukugo, 'a jukugo to be a Hash', Hash) do
     attr_is(jukugo, :word, String) + attr_is(jukugo, :pronunciation, String) +
@@ -119,17 +143,23 @@ def jukugo_errors(jukugo)
   end
 end
 
+# @param jukugo [any]
+# @param [Boolean]
 def valid_jukugo?(jukugo)
   jukugo_errors(jukugo).empty?
 end
 
+# @param jukugo [Jukugo]
+# @raise if jukugo is not a valid Jukugo object
 def validate_jukugo(jukugo)
-  return valid_jukugo?(jukugo)
+  return if valid_jukugo?(jukugo)
 
   # If the jukugo structure is invalid here it's my fault, so raise an error
   raise "Invalid jukugo:\n" + jukugo_errors(jukugo).join("\n")
 end
 
+# @param kunyomi [any]
+# @return [Array<String>]
 def kunyomi_errors(kunyomi)
   obj_is(kunyomi, 'a kunyomi to be a Hash', Hash) do
     attr_is(kunyomi, :word, String) + attr_is(kunyomi, :pronunciation, String) +
@@ -137,10 +167,14 @@ def kunyomi_errors(kunyomi)
   end
 end
 
+# @param kunyomi [any]
+# @return [Boolean]
 def valid_kunyomi?(kunyomi)
   kunyomi_errors(kunyomi).empty?
 end
 
+# @param kunyomi [Kunyomi]
+# @raise if kunyomi is not a valid Kunyomi object
 def validate_kunyomi(kunyomi)
   return if valid_kunyomi?(kunyomi)
 
@@ -148,8 +182,12 @@ def validate_kunyomi(kunyomi)
   raise "Invalid kunyomi:\n" + kunyomi_errors(kunyomi).join("\n").inspect
 end
 
-def maybe_wraps_a(hash, attr, expected_klass)
-  attr_is(hash, attr, May::Be) do |m|
+# @param data [any]
+# @param attr @TODO
+# @param expected_klass [Class]
+# @return [Array<String>]
+def maybe_wraps_a(data, attr, expected_klass)
+  attr_is(data, attr, May::Be) do |m|
     klass = m.map(&:class).get_or_else_value(expected_klass)
     if klass == expected_klass
       []
@@ -161,6 +199,8 @@ def maybe_wraps_a(hash, attr, expected_klass)
   end
 end
 
+# @param data [any]
+# @return [Array<String>]
 def page_data_errors(data)
   obj_is(data, 'a page_data to be a Hash', Hash) do
     attr_is(data, :index, Integer) + component_errors(data[:character]) +
@@ -180,15 +220,19 @@ def page_data_errors(data)
   end
 end
 
+# @param data [any]
+# @return [Boolean]
 def valid_page_data?(data)
   page_data_errors(data).empty?
 end
 
+# @param data [PageData]
+# @raise if data is not a valid PageData object
 def validate_page_data(data)
+  return if valid_page_data?(data)
+
   # If the data is invalid here it's my fault, so raise an error
-  unless valid_page_data?(data)
-    raise "Invalid page data:\n" + page_data_errors(data).join("\n")
-  end
+  raise "Invalid page data:\n" + page_data_errors(data).join("\n")
 end
 
 # @param arr [Array<A>]
@@ -197,6 +241,14 @@ def head(arr)
   arr.first ? May::Some.new(arr.first) : May::None.new
 end
 
+# @param str [String]
+# @return [May::Be<Integer>]
+def str_to_int(str)
+  str == str.to_i.to_s ? May::Some.new(str.to_i) : May::None.new
+end
+
+# @param html [Mechanize::Page]
+# @return [May::Be<Integer>]
 def get_page_index(html)
   text_at(html, '.navigation-header > .text-centered').and_then do |str|
     head(str.match(/Number\s+(\d+)/m).to_a.reverse)
@@ -204,6 +256,8 @@ def get_page_index(html)
     .and_then { |str| str_to_int(str) }
 end
 
+# @param html [Mechanize::Page]
+# @return [May::Be<String>]
 def get_character(html)
   text_at(html, 'h1 > .kanji_character').map { |k| kanji_component(k) }
     .or_else do
@@ -216,14 +270,15 @@ end
 # @param html [Mechanize::Page]
 # @return [Array<Component>]
 def get_kanji_components(html)
-  # TODO: Will this always work?
   html.search('.span8 > .component').map do |c|
     if c.text.empty?
-      radical_component(c.search('[alt]').first['alt'])
+      head(c.search('[alt]')).map { |a| radical_component(a['alt']) }
+        .get_or_else_value(nil)
     else
       kanji_component(c.text)
     end
   end
+    .compact
 end
 
 # @param html [Mechanize::Page]
@@ -232,12 +287,21 @@ def get_onyami(html)
   table_under_heading(html, 'Onyomi').and_then { |t| text_at(t, 'td', &:first) }
 end
 
+# @param node [Nokogiri::XML::Node]
+# @return [May::Be<Nokogiri::XML::Node>]
+def maybe_next(node)
+  sibling = node.next_sibling
+  sibling ? May::Some.new(sibling) : May::None.new
+end
+
 # @param html [Mechanize::Page]
 # @param heading [String]
 # @return [May::Be<Nokogiri::XML::Element>]
 def table_under_heading(html, heading)
-  head(html.search('h2').select { |h2| h2.text == heading }).map(&:next_sibling)
-    .map(&:next_sibling)
+  head(html.search('h2').select { |h2| h2.text == heading }).and_then do |n|
+    maybe_next(n)
+  end
+    .and_then { |n| maybe_next(n) }
     .and_then { |el| el.name == 'table' ? May::Some.new(el) : May::None.new }
 end
 
@@ -266,6 +330,7 @@ def get_mnemonic(html)
 end
 
 # @param html [Mechanize::Page]
+# @param character [Component]
 # @return [Array<Kunyomi>]
 def get_kunyomi(html, character)
   return [] unless character[:kind] == 'kanji'
@@ -321,10 +386,6 @@ def get_next_page_link(html)
   next_page ? May::Some.new(next_page.resolved_uri) : May::None.new
 end
 
-def str_to_int(str)
-  str == str.to_i.to_s ? May::Some.new(str.to_i) : May::None.new
-end
-
 # @param html [Mechanize::Page]
 # @return [May::Be<PageData>]
 def get_page_data(html)
@@ -357,6 +418,9 @@ def data_at(agent, uri)
   visit(agent, uri).and_then { |page| get_page_data(page) }
 end
 
+# @param agent [Mechanize]
+# @param pages [Array<Mechanize::Page>]
+# @return [Array<Mechanize::Page>]
 def fill_pages(agent, pages)
   return pages if pages.empty?
 
@@ -366,6 +430,7 @@ def fill_pages(agent, pages)
     .get_or_else_value(pages)
 end
 
+# Scrape KanjiDamage.com and create a JSON file of the data
 def main
   agent = Mechanize.new
 
@@ -374,9 +439,8 @@ def main
 
   pages = fill_pages(agent, pages)
 
-  pages_json = JSON.pretty_generate(pages)
-
-  File.write(SAVE_FILE, pages_json)
+  File.write(SAVE_FILE, pages.to_json)
 end
 
+# It's all come to this!
 main
